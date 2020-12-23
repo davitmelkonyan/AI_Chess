4908c9b3cd097e818d6e1a6024bb0d1ec663559e
Davit Melkonyan <dmelkony@uci.edu>
2020-12-23 14:49:39 -0800

    pawn almost finished

--- python/chessPlayer.py
+++ python/chessPlayer.py
@@ -12,18 +12,17 @@ def main():
     game.update_All()
     game.after_Update()
     ###################
+    mPos = (0,0)
+    mOffset = (0,0)
     screenSize = (360,360)
     display = pygame.display.set_mode(screenSize)
     pygame.display.set_caption("Chess")
+    clock = pygame.time.Clock()
     ###################
     runGame = True
     time = 0
-    clock = pygame.time.Clock()
     pieceInHand = None
-    mPos = (0,0)
-    mOffset = (0,0)
     print(game)
-    ###################
     while(runGame):
         blackMove = False
         mPos = pygame.mouse.get_pos()
@@ -38,7 +37,7 @@ def main():
                     if(event.type == 5):
                         #Mouse click
                         i1 = game.getPieceAt((int(mPos[0]/45),int(mPos[1]/45)))
-                        if(i1 and i1.team == game.currentTeam):
+                        if(i1 and i1.color == game.currentTeam):
                             mOffset = (int(mPos[0]%45),int(mPos[1]%45))
                             if(pieceInHand):
                                 pieceInHand.canRender = True
@@ -78,9 +77,9 @@ def pos2index(pos):
     return pos[0]+pos[1]*8
 
 class piece:
-    pos = (0,0)
+    loc = (0,0)
     board = None
-    team = -1
+    color = -1
     spritesheet = (pygame.image.load("chesspieces.png"),45)
     spriteIndex = (0,0)
 
@@ -93,9 +92,9 @@ class piece:
     semiThreat = []
     char = ["?"]
 
-    def __init__(self,board,pos,team):
-        self.pos = pos
-        self.team = team
+    def __init__(self,board,loc,color):
+        self.loc = loc
+        self.color = color
         self.board = board
         self.canRender = True
         self.hasMoved = False
@@ -104,26 +103,26 @@ class piece:
         self.validMoves = []
         self.semiThreat = []
 
-    def moveTo(self,pos):
-        if(self.canMoveTo(pos)):
+    def moveTo(self,loc):
+        if(self.canMoveTo(loc)):
             self.hasMoved = True
-            pos2 = self.board.getPieceAt(pos)
-            self.board.setPieceAt(pos,self)
-            self.board.setPieceAt(self.pos,None)
-            self.pos = pos
-            if(pos2):
-                pos2.kill()
+            destPiece = self.board.getPieceAt(loc)
+            self.board.setPieceAt(loc,self)
+            self.board.setPieceAt(self.loc,None)
+            self.loc = loc
+            if(destPiece): #kills opponent
+                destPiece.kill()
                 return True,True
             return True,False
         return False,False
 
-    def canMoveTo(self,pos):
-        return pos in self.validMoves
+    def canMoveTo(self,loc):
+        return loc in self.validMoves
     
     def render(self,surface):
         if(self.canRender):
             s = self.spritesheet
-            surface.blit(s[0],(self.pos[0]*s[1],self.pos[1]*s[1]),((self.spriteIndex[self.team]%6)*s[1],math.floor(self.spriteIndex[self.team]/6)*s[1],s[1],s[1]))
+            surface.blit(s[0],(self.loc[0]*s[1],self.loc[1]*s[1]),((self.spriteIndex[self.color]%6)*s[1],math.floor(self.spriteIndex[self.color]/6)*s[1],s[1],s[1]))
             
     def kill(self):
         pass
@@ -132,7 +131,7 @@ class King(piece):
     spriteInd = (0,6)
 
     def render(self,surface):
-        if self.board.isThreatend(self.pos, self.team):
+        if self.board.isThreatend(self.pos, self.color):
             pygame.draw.rect(surface,(255,100,0),(self.pos[0]*45,self.pos[1]*45,45,45))
         super(King,self).render(surface)
     
@@ -147,6 +146,84 @@ class Bishop(piece):
 
 class Pawn(piece):
     char = ["P","p"]
+    doubleMovesDone = False
+
+    def __init__(self, board, loc, color):
+        self.validMoves = []
+        self.threat = []
+        self.doubleMovesDone = False
+        piece.__init__(self,  board, loc, color,color)
+
+    def move(self, loc):
+        xDiff = loc[0] - self.loc[0]
+        yDiff = loc[1] - self.loc[1]
+        empty, kill = super(Pawn, self).moveTo(loc)
+        self.doubleMovesDone = abs(yDiff)>1 and empty
+
+        if (empty):
+            #en passant condition(enemy pawns right next to each other) = enemy can kill as if pawn moved 1 step forwrad instead of 2 
+            if (not kill and abs(yDiff)==1 and abs(xDiff)==1): 
+                opponentPos = (loc[0], loc[1] + (self.color*2-1))
+                opponentPiece = self.board.getPiecesAt(opponentPos)
+                self.board.setPieceAt((opponentPos,None)) #removes the enemy next to it but still moves to its normal destination (diagonaly)
+                opponentPiece.kill()
+            
+            #pawn reaches end of board(transfroms)
+            if (loc[1]+1)%8 == self.color:
+                while(True): 
+                    try:
+                        if self.color == 1:
+                            self.board.setPieceAt(loc, Queen(self.board,loc,self.color))
+                        inp = int(input("Change pawn to 1: queen, 2: knight, 3: bishop, 4: rook: "))
+                        if(inp == 1):
+                            obj = Queen(self.board,loc,self.color)
+                        if(inp == 2):
+                            obj = Knight(self.board,loc,self.color)
+                        if(inp == 3):
+                            obj = Bishop(self.board,loc,self.color)
+                        if(inp == 4):
+                            obj = Rook(self.board,loc,self.color)
+                        self.board.setPieceAt(loc,obj)
+                        break
+                    except Exception: 
+                        pass
+        return empty, kill
+
+        def update(self):
+            pos = self.pos
+            self.threat = [(pos[0]-1,pos[1]-(self.color*2 - 1)),(pos[0]+1,pos[1]-(self.color*2 - 1))]
+
+            ###########Update Valid moves############
+            self.validMoves = []
+            for i in self.threat:
+                piece = self.board.getPieceAt(i)
+                if(piece):
+                    if(piece.team != self.team): #check if enemy
+                        self.validMoves += [i] #can eat
+            
+            '''
+            p1 = (pos[0],self.pos[1]-(self.team*2 - 1))
+            p2 = (pos[0],self.pos[1]-(self.team*2 - 1)*2)
+            
+            if(not self.board.getPieceAt(p1)):
+                self.validMoves += [p1]
+            if(not self.hasMoved):
+                if(not self.board.firstEncounter(pos,p2)):
+                    self.validMoves += [p2]
+            
+            p3 = (pos[0]-1,pos[1])
+            p4 = (pos[0]+1,pos[1])
+            i3 = self.board.getPieceAt(p3)
+            i4 = self.board.getPieceAt(p4)
+            
+            if(i3 and i3.hadLastMove and isinstance(i3,Pawn) and i3.movedTwice and i3.team != self.team):
+                self.validMoves += [(pos[0]-1,pos[1]-(self.team*2 - 1))]
+            elif(i4 and i4.hadLastMove and isinstance(i4,Pawn) and i4.movedTwice and i4.team != self.team):
+                self.validMoves += [(pos[0]+1,pos[1]-(self.team*2 - 1))]'''
+
+
+
+        
 
 class Queen(rook,bishop):
     char = ["Q","q"]
